c4478683ed4a3096574c2462d0e82ec04183b929
cypressworld <95178527+cypressworld@users.noreply.github.com>
2026-07-03 14:08:58 +0100

    Add files via upload

---
+++ src/main/java/com/exam/saas/model/Question.java
@@ -0,0 +1,20 @@
+package com.exam.saas.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Question{
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 1000)
+    private String QuestionText;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
+    private String correctAnswer;
+
+}
